9c6a81aa1665d12b5cff659a60822111a70d8f50
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-05-09 22:23:02 -0300

    terminamos de criar a pagina de detalhes. vamos fazer a navegacao de home para detail

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notes = [
+    {
+      id: '1',
+      date: '2016-02-01',
+      title: 'Firebase',
+      content: 'Que tal programar Serverless?'
+    },
+    {
+      id: '2',
+      date: '2016-01-01',
+      title: 'Ionic',
+      content: 'Aprenda o básico de Ionic'
+    },
+    {
+      id: '3',
+      date: '2016-03-01',
+      title: 'Angular',
+      content: 'Importante para desenvolver com Ionic'
+    }
+  ]
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  itemClicado(note){
+    console.log('itemClicado', note);
 
   }
 
